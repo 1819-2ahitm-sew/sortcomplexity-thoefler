0866aa759a608bfe1ea2faea88e700b0c51cb232
tobiashoefler <tobiashoefl@gmail.com>
2019-03-29 10:50:51 +0100

    first commit

--- src/main/java/at/htl/SortComplexity.java
+++ src/main/java/at/htl/SortComplexity.java
@@ -1,14 +1,58 @@
 package at.htl;
 
+import java.util.Scanner;
+
 public class SortComplexity {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int amountArrays = 0, arraySize = 0;
+
+        System.out.printf("%nAnzahl der zu sortierenden Arrays: ");
+        amountArrays = scanner.nextInt();
+
+        System.out.println();
+        System.out.printf("Größe der Arrays: ");
+        arraySize = scanner.nextInt();
+
+        for (int i = 0; i < amountArrays; i++) {
+
+            sortRandomArray(arraySize);
+            
+        }
+
+        
 
     }
 
     // Sorts an array of random numbers and returns the sorting run time in ms
     public static long sortRandomArray(int length) {
+
+        int[] array = new int[length];
+        boolean switched;
+        int i = array.length - 1;
+
+        for (int x = 0; x <= length; i++) {
+            array[i] = (int) (Math.random() * (0 - 100)) + 100;
+        }
+
+        do {
+            switched = false;
+
+            for (int j = 0; j < i; i++) {
+                if (array[j] > array[j+1]) {
+                    int help = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = help;
+                    switched = true;
+                }
+            }
+            i--;
+        } while (switched || (i >= 0));
+
+
         return 0;
     }
 
